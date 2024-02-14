4a5d98b0aa87b8c7c07ef04b68855e65bbe3f054
HertelendyKristof <hertelendy.kristof@gmail.com>
2024-02-14 18:46:28 +0100

    Működik a fetchem

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,17 +1,10 @@
-import {useEffect, useState} from 'react'
 import './App.css'
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import Register from "./Register/Register.tsx";
 import About from "./Register/About.tsx";
 
 function App() {
-  const [_, setGreeting] = useState('');
 
-    useEffect(() => {
-        fetch('/api')
-            .then((res) => res.text())
-            .then(setGreeting)
-    })
   return (
     <>
         <BrowserRouter>
--- apps/client/src/Register/Register.tsx
+++ apps/client/src/Register/Register.tsx
@@ -1,58 +1,70 @@
-import { useState} from "react";
+import {FormEvent, useState} from "react";
 import {Link} from "react-router-dom";
 
 function Register() {
 
-    const [,] = useState('');
+        const [username, setUsername] = useState('');
+        const [email, setEmail] = useState('');
+        const [password, setPassword] = useState('');
 
-    function sendData() {
-        const nameInput = document.getElementById("Username") as HTMLInputElement;
-        const emailInput = document.getElementById("Email") as HTMLInputElement;
-        const passwordInput = document.getElementById("Password") as HTMLInputElement;
-        const cPasswordInput = document.getElementById("CorrectPassword") as HTMLInputElement;
+        function sendData(event: FormEvent<HTMLFormElement>) {
+            event.preventDefault();
 
-        const name = nameInput.value
-        const email = emailInput.value
-        const password = passwordInput.value
-        const correctPassword = cPasswordInput.value
-
-        fetch('/api/register', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application.json'
-            },
-            body: JSON.stringify({name, email, password, correctPassword})
-        })
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error("Rosszul adtad meg az adatokat")
-                } else {
-                    return res.json()
+            fetch('/api/register', {
+                method: 'POST',
+                body: JSON.stringify({ username, email, password }),
+                headers:{
+                    'Content-type':'application/json'
                 }
             })
-            .then(data => {
-                console.log(data)
-            })
-    }
+                .then((res) => {
+                    if (!res.ok) {
+                        throw new Error("Rosszul adtad meg az adatokat");
+                    } else {
+                        return res.json();
+                    }
+                })
+                .then(data => {
+                    console.log(data);
+                })
+                .catch(error => {
+                    console.error('Hiba történt a fetch kérés során:', error);
+                });
+        }
 
     return (
         <>
-            <form method={"post"} action={"/register-success"}>
+            <form onSubmit={sendData}>
                 <p>Felhasználó név</p>
-                <input type={"text"}  id={"Username"}/>
+                <input
+                    type={"text"}
+                    name={"name"}
+                    id={"Username"}
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
                 <p>Email cím</p>
-                <input type={"email"} id={"Email"}/>
+                <input
+                    type={"email"}
+                    name={"email"}
+                    id={"Email"}
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
                 <p>Jelszó</p>
-                <input type={"password"} id={"Password"}/>
-                <p>Jelszó megerősítése</p>
-                <input type={"password"} id={"CorrectPassword"}/>
+                <input
+                    type={"password"}
+                    name={"password"}
+                    id={"Password"}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
                 <br/>
-                <Link to={"/"}>
-                    <input type={"submit"} onClick={sendData}>Regisztrálás</input>
-                </Link>
+                <input type={"submit"} value={"Regisztrálás"}/>
             </form>
+            <Link to={"/"}>Vissza a főoldalra</Link>
         </>
     )
-}
+    }
 
-export default Register;
+    export default Register;
