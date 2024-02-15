dfe602a5bb52d02012696c9b2d1ecd2142632133
Major Dominik <major.dominik0505@gmail.com>
2024-02-15 09:05:40 +0100

    password hashing regisztrációnál

--- apps/api/src/register/register.service.ts
+++ apps/api/src/register/register.service.ts
@@ -1,6 +1,8 @@
 import {BadRequestException, Injectable} from "@nestjs/common";
 import {Prisma, User} from "@prisma/client";
 import {PrismaService} from "../prisma.service";
+import * as bcrypt from 'bcrypt'
+import {hash} from "bcrypt";
 
 
 @Injectable()
@@ -9,8 +11,14 @@ export class RegisterService {
     constructor(private prisma: PrismaService) {
     }
     async createUser(data: Prisma.UserCreateInput): Promise<User | null> {
+
+
         return this.prisma.user.create({
-            data,
+            data: {
+                username: data.username,
+                email: data.email,
+                password: await bcrypt.hash(data.password, 10)
+            },
         })
     }
 
