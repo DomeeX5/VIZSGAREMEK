1fc47653c49b2880d72a88aa708e5a3e34c1253c
Major Dominik <major.dominik0505@gmail.com>
2024-02-20 08:15:30 +0100

    Revert "Revert "proba navbar megcsinalasa es melle letoltve a bootstrap melle""
    This reverts commit ed69d0a2a03eb0279cb0f1d63a7bbb9753027c2c.

--- apps/api/prisma/migrations/20240213195048_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `product_pic` to the `Product` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `product` ADD COLUMN `product_pic` LONGBLOB NOT NULL;
--- apps/api/prisma/migrations/20240215155649_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `user` MODIFY `Privilages_priv_id` INTEGER NULL DEFAULT 20;
