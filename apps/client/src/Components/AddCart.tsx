4fb309441e37ff362a5b271f0535d51188866aff
Major Dominik <major.dominik0505@gmail.com>
2024-04-06 17:39:49 +0200

    AddToCart contextre konvertálása

--- apps/client/src/Components/AddCart.tsx
+++ apps/client/src/Components/AddCart.tsx
@@ -1,21 +1,42 @@
-import { useState } from 'react';
+import React, {createContext, PropsWithChildren, useContext, useState} from 'react';
 import { CartItem } from '@prisma/client';
-import {useAuth} from "./Login/AuthContextProvider.tsx";
+import { useAuth } from './Login/AuthContextProvider.tsx';
+import { useCart } from './Cart/CartContext.tsx';
 
-function useAddToCart() {
-    const {isLoggedIn} = useAuth();
-    const [addCart, setAddCart] = useState<CartItem[]>();
+interface AddToCartContextType {
+    addCart?: CartItem[];
+    addToCart: (productId: number, quantity: number) => void;
+    showAlert: boolean;
+    setShowAlert: React.Dispatch<React.SetStateAction<boolean>>;
+    errors: string[];
+}
+
+const AddToCartContext = createContext<AddToCartContextType | undefined>(undefined);
+
+export const useAddToCart = () => {
+    const context = useContext(AddToCartContext);
+    if (!context) {
+        throw new Error('useAddToCart must be used within a AddToCartProvider');
+    }
+    return context;
+};
+
+export const AddToCartProvider= ({ children }: PropsWithChildren) => {
+    const { isLoggedIn } = useAuth();
+    const [addCart, setAddCart] = useState<CartItem[] | undefined>();
     const [showAlert, setShowAlert] = useState(false);
     const [errors, setErrors] = useState<string[]>([]);
-    const addToCart = (productId: number, quantity: number) => {
+    const {fetchCartData} = useCart()
+
+    const addToCart = async (productId: number, quantity: number) => {
         if (!isLoggedIn) {
             console.log('Nincs accessToken a sessionStorage-ben');
             setShowAlert(true);
             return;
         }
         const data = { productId, quantity };
-        const accessToken = sessionStorage.getItem("token")
-        fetch(`/api/cart/add`, {
+        const accessToken = sessionStorage.getItem('token');
+        await fetch(`/api/cart/add`, {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
@@ -30,6 +51,7 @@ function useAddToCart() {
                 } else {
                     const data = await res.json();
                     setAddCart(data);
+                    fetchCartData()
                 }
             })
             .catch((error) => {
@@ -38,7 +60,10 @@ function useAddToCart() {
             });
     };
 
-    return { addCart, addToCart, showAlert, setShowAlert, errors };
-}
 
-export default useAddToCart;
+    return (
+        <AddToCartContext.Provider value={{ addCart, addToCart, showAlert, setShowAlert, errors }}>
+            {children}
+        </AddToCartContext.Provider>
+    );
+};
