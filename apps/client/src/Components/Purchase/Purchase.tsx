62cb4210015f141d432f6d0e7be476421e9e9ffb
HertelendyKristof <hertelendy.kristof@gmail.com>
2024-04-08 14:03:17 +0200

    Purchase.tsx design elkezdese

--- apps/client/src/Components/Purchase/Purchase.tsx
+++ apps/client/src/Components/Purchase/Purchase.tsx
@@ -4,7 +4,6 @@ import Box from "@mui/material/Box";
 import Stepper from "@mui/material/Stepper";
 import StepLabel from "@mui/material/StepLabel";
 import Step from "@mui/material/Step";
-import Typography from "@mui/material/Typography";
 import { Button, MenuItem, Select, TextField } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
@@ -20,8 +19,16 @@ function Purchase() {
     const [activeStep, setActiveStep] = useState(0);
     const [_, setErrors] = useState<string>();
     const navigate = useNavigate();
-
     const steps = ['Szállítás helye', 'Fizetés', 'Adatok ellenőrzése'];
+    const [showHelperText, setShowHelperText] = useState(false);
+
+    const handleFocus = () => {
+        setShowHelperText(true);
+    };
+
+    const handleBlur = () => {
+        setShowHelperText(false);
+    };
 
     const handlePaymentTypeChange = (event: React.ChangeEvent<{ value: unknown }>) => {
         setPaymentType(event.target.value as string);
@@ -69,14 +76,11 @@ function Purchase() {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
     };
 
-    const handleReset = () => {
-        setActiveStep(0);
-    };
 
     return (
         <>
             <div className={"container"}>
-                <Box sx={{width: '100%', minHeight: '500px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
+                <Box sx={{width: '75%', height: '400px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
                     <Stepper activeStep={activeStep}>
                         {steps.map((label) => (
                             <Step key={label}>
@@ -87,24 +91,33 @@ function Purchase() {
                     <div>
                         {activeStep === 0 && (
                             <>
+                                <h1 style={{margin:'10px'}}>Adatok megadása</h1>
                                 <div className={"break"}></div>
-                                <TextField label="Country" value={address.country} onChange={(e) => handleAddressChange(e, 'country')} fullWidth />
-                                <TextField label="State" value={address.state} onChange={(e) => handleAddressChange(e, 'state')} fullWidth />
-                                <TextField label="City" value={address.city} onChange={(e) => handleAddressChange(e, 'city')} fullWidth />
-                                <TextField label="Street" value={address.street} onChange={(e) => handleAddressChange(e, 'street')} fullWidth />
-                                <TextField label="House Number" value={address.house_number} onChange={(e) => handleAddressChange(e, 'house_number')} fullWidth />
-                                <div className={"break"}></div>
+                                <TextField label="Country" value={address.country} onChange={(e) => handleAddressChange(e, 'country')} style={{margin:'0 0 0 10px'}}   helperText={showHelperText ? "Add meg az országod nevét" : ""} required onFocus={handleFocus} onBlur={handleBlur}/><br/>
+                                <TextField label="State" value={address.state} onChange={(e) => handleAddressChange(e, 'state')} style={{margin:'10px'}} helperText={showHelperText ? "Add meg a megyédnek a nevét":""} required onFocus={handleFocus} onBlur={handleBlur} /><br/>
+                                <TextField label="City" value={address.city} onChange={(e) => handleAddressChange(e, 'city')}  style={{margin:'10px'}} helperText={showHelperText ?"Addja meg hogy melyik városban lakik":""} required onFocus={handleFocus} onBlur={handleBlur} /><br/>
+                                <TextField label="Street" value={address.street} onChange={(e) => handleAddressChange(e, 'street')}  style={{margin:'10px'}} helperText={showHelperText ?"Addja meg melyik utcáját":""} required onFocus={handleFocus} onBlur={handleBlur} /><br/>
+                                <TextField label="House Number" value={address.house_number} onChange={(e) => handleAddressChange(e, 'house_number')} style={{margin:'10px'}} helperText={showHelperText ?"Addja meg a házszámát":""} required onFocus={handleFocus} onBlur={handleBlur}/><br/>
                             </>
                         )}
                         {activeStep === 1 && (
                             <>
                                 <div className={"break"}></div>
-                                <TextField label="Kártyaszám" type="text" fullWidth />
-                                <TextField label="Lejárati idő" type="text" fullWidth />
-                                <TextField label="Tulajdonos neve" type="text" fullWidth />
-                                <TextField label="CVV" type="text" fullWidth />
-                                <div className={"break"}></div>
-                                <Select value={paymentType} onChange={() => handlePaymentTypeChange} displayEmpty fullWidth variant="outlined">
+                                <TextField label="Kártyaszám" type="text" style={{margin:'0 0 0 10px'}}  helperText={showHelperText ?"Adja meg a kártyaszámát. PL: 1234 1234 1234 1234":""} required onFocus={handleFocus} onBlur={handleBlur} fullWidth />
+                                <TextField label="Lejárati idő" type="text" style={{margin:'0 0 0 10px'}}  helperText={showHelperText ?"Adja meg a kártyája lejárati dátumát":""} required onFocus={handleFocus} onBlur={handleBlur} fullWidth />
+                                <TextField label="Tulajdonos neve" type="text" style={{margin:'0 0 0 10px'}}  helperText={showHelperText ?"Adja meg a kártya elején lévő tulajdonos nevét.":""} required onFocus={handleFocus} onBlur={handleBlur} fullWidth />
+                                <TextField label="CVV" type="text" style={{margin:'0 0 0 10px'}}  helperText={showHelperText ?"Fordítsa meg a kártyáját és látni fog egy háromjegyű számot azt adja meg. Pl:123":""} required onFocus={handleFocus} onBlur={handleBlur} fullWidth />
+                                <Select value={paymentType}
+                                        style={{margin:'0 0 0 10px'}}
+                                        helperText={showHelperText ?"Válassza ki hogy hogyan fizet":""}
+                                        onFocus={handleFocus}
+                                        onBlur={handleBlur}
+                                        onChange={handlePaymentTypeChange}
+                                        displayEmpty
+                                        required
+                                        fullWidth
+                                        variant="outlined"
+                                >
                                     <MenuItem value="10">Utalásos fizetés</MenuItem>
                                     <MenuItem value="20">Bankártyás fizetés</MenuItem>
                                     <MenuItem value="30">Átvételes fizetés</MenuItem>
@@ -118,15 +131,6 @@ function Purchase() {
                             </>
                         )}
                     </div>
-                    {activeStep === steps.length ? (
-                        <React.Fragment>
-                              <Typography sx={{mt: 2, mb: 1}}>All steps completed - you&apos;re finished</Typography>
-                            <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
-                                <Box sx={{flex: '1 1 auto'}} />
-                                <Button onClick={handleReset}>Reset</Button>
-                            </Box>
-                        </React.Fragment>
-                    ) : (
                         <React.Fragment>
                             <Box sx={{display: 'flex', flexDirection: 'row', pt: 2}}>
                                 <Button color="inherit" disabled={activeStep === 0} onClick={handleBack} sx={{mr: 1}}>Vissza</Button>
@@ -134,8 +138,7 @@ function Purchase() {
                                 <Button onClick={handleNext}>{activeStep === steps.length - 1 ? 'Vásárlás' : 'Következő'}</Button>
                             </Box>
                         </React.Fragment>
-                    )}
-                </Box>
+                    </Box>
             </div>
         </>
     );
