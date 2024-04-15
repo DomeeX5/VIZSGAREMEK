1628bbf44c7bbc572c424187139fc9a22ce4c265
Major Dominik <major.dominik0505@gmail.com>
2024-04-15 10:49:43 +0200

    Prismával kapcsolatos fájlok dokumentálása typedoc-al

--- apps/api/src/prisma/prisma.module.ts
+++ apps/api/src/prisma/prisma.module.ts
@@ -2,6 +2,9 @@ import {Module} from "@nestjs/common";
 import {PrismaService} from "./prisma.service";
 
 
+/**
+ * Module for providing the Prisma service throughout the application.
+ */
 @Module({
     providers: [PrismaService],
     exports: [PrismaService]
--- apps/api/src/prisma/prisma.service.ts
+++ apps/api/src/prisma/prisma.service.ts
@@ -1,8 +1,15 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+/**
+ * Service for interacting with the Prisma ORM.
+ * Extends PrismaClient from the Prisma client library.
+ */
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+    /**
+     * Initializes the Prisma client upon module initialization.
+     */
     async onModuleInit() {
         await this.$connect();
     }
